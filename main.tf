128377be405a1bfc0ac7f763310b648a73d68f7d
Omer Revach <155072562+BrushingMonkey@users.noreply.github.com>
2024-09-02 10:22:56 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,77 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 0.12.0"
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.SG.id]
+
+  user_data = file("user_data.sh")
+
+  tags = {
+    Name = "github-actions"
+  }
+}
+
+resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "SG" {
+  name        = "Grafana"
+  description = "Allow web inbound traffic"
+  vpc_id = aws_default_vpc.default.id
+
+  dynamic "ingress" {
+    for_each = var.allow_ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "instance_public_ip" {
+  value = aws_instance.web.public_ip
+}
